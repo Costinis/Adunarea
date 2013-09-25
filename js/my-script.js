e2b7d73f3d256ffacc91d32eb9a093b7b7495ae2
Costinis <nistor_costi@yahoo.com>
2013-09-25 15:58:12 +0300

    up3

--- js/my-script.js
+++ js/my-script.js
@@ -24,47 +24,11 @@ $(document).ready(function(e) {
 	  $('.sum0r6').hide();
 	  $('.sum_r6, .check6').show();
 	});
-
-
-
-
-    
-});
-
-$(document).ready(function(e) {   
-	$('.correct1').click(function() {
-	  $('.sum0r1').hide();
-	  $('.sum_r1, .check1').show();
-	});
-	$('.correct2').click(function() {
-	  $('.sum0r2').hide();
-	  $('.sum_r2, .check2').show();
-	});
-	$('.correct3').click(function() {
-	  $('.sum0r3').hide();
-	  $('.sum_r3, .check3').show();
-	});
-	$('.correct4').click(function() {
-	  $('.sum0r4').hide();
-	  $('.sum_r4, .check4').show();
-	});
-	$('.correct5').click(function() {
-	  $('.sum0r5').hide();
-	  $('.sum_r5, .check5').show();
-	});
-    $('.correct6').click(function() {
-	  $('.sum0r6').hide();
-	  $('.sum_r6, .check6').show();
-	});
-
-
-
-
     
 });
 
  // Sound effect
- //function EvalSound(soundobj) {
-  //var thissound=document.getElementById(soundobj);
- // thissound.play();
-//}
+ function EvalSound(soundobj) {
+  var thissound=document.getElementById(soundobj);
+  thissound.play();
+}
