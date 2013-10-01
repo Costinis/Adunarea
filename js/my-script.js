da554e9108c780b632db3e892fc423443a8ae081
Costinis <nistor_costi@yahoo.com>
2013-10-01 15:05:29 +0300

    up4

--- js/my-script.js
+++ js/my-script.js
@@ -28,7 +28,23 @@ $(document).ready(function(e) {
 });
 
  // Sound effect
- function EvalSound(soundobj) {
-  var thissound=document.getElementById(soundobj);
-  thissound.play();
-}
+
+     function playAudio(src) {
+       if (device.platform == 'Android') {
+        src = '/android_asset/www/sound/' + src;
+       }
+ 
+     var media = new Media(src, success, error_error);
+ 
+     media.play();
+     }
+ 
+     function success() {
+     // ignore
+     }
+ 
+     function error_error(e) {
+     alert('great error');
+     alert(e.message);
+   }
+
